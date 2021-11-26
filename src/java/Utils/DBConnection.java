0b8a07f2967c809d0e297837d1b3b4600a225ec3
WilliamTrung <52268132+WilliamTrung@users.noreply.github.com>
2021-11-26 13:26:58 +0700

    final update to the project
    add documents
    add database generating script files

--- src/java/Utils/DBConnection.java
+++ src/java/Utils/DBConnection.java
@@ -24,7 +24,7 @@ public class DBConnection {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         String url = "jdbc:sqlserver://localhost:1433;databaseName=FPTEventManagement";
-        Connection conn = DriverManager.getConnection(url, "sa", "12");
+        Connection conn = DriverManager.getConnection(url, "sa", "123");
         return conn;
     }
     public static Connection getConnection1() throws NamingException,SQLException{
